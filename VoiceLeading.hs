9b159f7608e5c7701aab514e5591eb23cfee22c0
sdclibbery <sdclibbery@googlemail.com>
2015-02-12 00:11:10 +0000

    Support for testing part ranges

--- VoiceLeading.hs
+++ VoiceLeading.hs
@@ -4,9 +4,9 @@ Description : Voice leading analysis for music
 -}
 
 module VoiceLeading (
-	Rule(..),
-	Result(..),
-	tryAddNote
+  Rule(..),
+  Result(..),
+  tryAddNote
 ) where
 import Structure
 import Note
@@ -18,6 +18,13 @@ data Rule = PartRange deriving (Show, Eq)
 data Result = Ok | Error Rule | Warning Rule deriving (Show, Eq)
 
 tryAddNote :: Music -> PartName -> Event -> Result
-tryAddNote m p e = Ok
+tryAddNote m p e = partRange p e 
 
-
+partRange :: PartName -> Event -> Result
+partRange p (Play _ n) = checkRange n $ range p
+    where
+      checkRange n (lo, hi) = if n >= lo && n <= hi then Ok else Error PartRange
+      range Treble = (Note C Nat 4, Note C Nat 6)
+      range Alto   = (Note G Nat 3, Note F Nat 5)
+      range Tenor  = (Note C Nat 3, Note C Nat 5)
+      range Bass   = (Note E Nat 2, Note E Nat 4)
--- VoiceLeadingTests.hs
+++ VoiceLeadingTests.hs
@@ -10,8 +10,10 @@ main = runTestTT $ TestList
 
 
 partRange = TestList [
-  test Ok                 Treble c{-,
-  test (Error PartRange)  Treble c__-}
+  test Ok                 Treble g,
+  test (Error PartRange)  Treble c__,
+  test (Error PartRange)  Treble b'',
+  test (Error PartRange)  Bass g
   ] where
     test e p s = (show s ++ show p) ~: e ~=? tryAddNote m p s
     m = music [ [c_], [g_], [c], [e] ]
